0b9842eb4e1f1b137c8fce96c0420d64a93e08a4
Alejandro Garci­a <a21alejandrogl@iessanclemente.net>
2022-06-13 13:22:38 +0200

    Creamos a clase de probas para Conta

---
+++ ProxectoBanco/src/main/java/com/mycompany/proxectobanco/ProbaConta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proxectobanco;
+
+/**
+ *
+ * @author Alex
+ */
+public class ProbaConta {
+    
+      public static void main(String[] args) {
+        //Creamos un obxecto persoa co construtor sen parámetros
+        Conta Conta1=new Conta();
+        //poñémoslle nome ao obxecto persoa1
+    }    
+}
